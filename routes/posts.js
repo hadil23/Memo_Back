84989a784f4bb8cb21172ebae42dd65c74efc969
hadil23 <hadil.afi@isimg.tn>
2023-02-13 15:58:51 +0100

    update poste

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,6 @@ const router = express.Router();
 import {getPosts,createPost,updatePost, deletePost,likePost} from '../controllers/posts.js'
 
 router.get('/',getPosts);
-
 router.post('/',createPost);
 router.patch('/:id',updatePost);
 router.delete('/:id',deletePost);
