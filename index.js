988d66fb2836632efed25d596e4c78135c164d1e
hadil23 <hadil.afi@isimg.tn>
2023-02-13 15:53:42 +0100

    first push

---
+++ index.js
@@ -0,0 +1,20 @@
+import  express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postsRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+app.use('/posts',postsRoutes);
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT||5000;
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>app.listen(PORT,()=>console.log(`server runing on port: ${PORT}`)))
+.catch((error)=>console.log(error.message));
+
